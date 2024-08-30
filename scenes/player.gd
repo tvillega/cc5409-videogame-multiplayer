8053f023b5fbcd7277d4dd5682fcf7ca92ee0562
MortanTheAndal <martinr769@gmail.com>
2024-08-30 19:16:13 -0400

    new Tank character model and animations

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,26 +9,28 @@ var player
 @onready var label: Label = $Label
 @onready var input_synchronizer: InputSynchronizer = $InputSynchronizer
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-
+func _ready() -> void:
+	anim.play("idle_down")
+	
 func _input(event: InputEvent) -> void:
 	if is_multiplayer_authority():
 		if event.is_action_pressed("test"):
 			test.rpc()
-
+			
 
 func _physics_process(delta: float) -> void:
-	#if is_multiplayer_authority():
-	
-	var directions = input_synchronizer.directions 
+	if is_multiplayer_authority():
+		look_at(get_global_mouse_position())
+		
+	var directions = input_synchronizer.directions
 	velocity = directions * speed
-	look_at(get_global_mouse_position())
-	
-	if is_on_floor() and input_synchronizer.jump:
-		velocity.y = -jump_speed
+	if input_synchronizer.jump:
+		velocity = directions * jump_speed
 	input_synchronizer.jump = false
 	
-	# send_position.rpc(position, velocity)
+	send_position.rpc(position, velocity)
 	move_and_slide()
 
 
@@ -43,10 +45,9 @@ func setup(player_data: Statics.PlayerData) -> void:
 
 @rpc("authority", "call_local", "unreliable")
 func test():
-	Debug.log("test %s" % player.name)
+	Debug.log("player: %s directions: %s" % [player.name, player.directions])
 
-
-@rpc
+@rpc("authority")
 func send_position(pos: Vector2, vel: Vector2) -> void:
 	position = lerp(position, pos, 0.5)
 	velocity = lerp(velocity, vel, 0.5)
