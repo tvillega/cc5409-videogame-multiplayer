1ef5199f8f1ad117a379b32ccf7160d183135442
Tom Villegas <tvillega@dcc.uchile.cl>
2024-10-17 09:16:42 -0300

    feat: all enemies work by inheritance

--- scenes/npc.gd
+++ scenes/npc.gd
@@ -12,26 +12,31 @@ var target: Node2D
 @onready var detection_area: Area2D = $DetectionArea 
 
 func _process(_delta):
+	# Change sprite depending on what is going on
 	if target:
 		_animated_sprite.play("chase")
-		
 	else:
 		_animated_sprite.play("idle")
 
 func _ready() -> void:
+	# Run detection area stuff (only inside server)
 	if multiplayer.is_server():
 		detection_area.body_entered.connect(_on_body_entered)
 		detection_area.body_exited.connect(_on_body_exited)
-	
+
 func _physics_process(delta):
-	
+	# Start a chase towards the player
 	if target:
 		var direction = global_position.direction_to(target.global_position)
 		var distance = global_position.distance_to(target.global_position)
+		# Don't glitch the player movement
 		if distance > 50:
 			velocity = velocity.move_toward(direction * speed, acceleration * delta)
 			move_and_slide()
 	
+###
+### This sets the chase target i.e. the player
+###
 func _on_body_entered(body: Node) -> void:
 	var player = body as Player
 	if player:
@@ -45,8 +50,7 @@ func set_target(value: Node2D) -> void:
 	target = value
 	var path = target.get_path() if target else null
 	set_target_remote.rpc(path if path else null)
-	
-#func set_target_remote(value: EncodedObjectAsID):
+
 @rpc("any_peer", "reliable")
 func set_target_remote(target_path):
 	if target_path:
@@ -54,5 +58,8 @@ func set_target_remote(target_path):
 	else:
 		target = null
 
+##
+## This functions are called through signals by other entities
+##
 func take_damage(damage: int):
 	Debug.log("NPC says auch! -%d" % damage)
