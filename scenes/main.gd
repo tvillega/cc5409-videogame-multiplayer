fb2b09c3005444f809deb01cdaf54afdcede80f3
Tom Villegas <tvillega@dcc.uchile.cl>
2024-11-25 15:36:54 -0300

    refactor: scene mobs

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var player_scene: PackedScene
 @onready var players: Node2D = $Players
-@onready var markers: Node2D = $Markers
+@onready var markers: Node2D = $PlayersSpawnMarker
 #@onready var npc: Node2D = $npc
 
 func _ready() -> void:
