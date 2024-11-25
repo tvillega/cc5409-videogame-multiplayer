e88cfa02a54d588c04e9b34345b27ba285ad1a6a
Tom Villegas <tvillega@dcc.uchile.cl>
2024-11-25 15:45:29 -0300

    fix: crash when spawning skel

--- scenes/enemies/skel_spawner.gd
+++ scenes/enemies/skel_spawner.gd
@@ -1,6 +1,6 @@
 extends MultiplayerSpawner
 
-@onready var marker_2d: Marker2D = $Marker2D
+@onready var marker_2d: Marker2D = $SkelSpawnerMarker
 @onready var timer: Timer = $Timer
 @export var enemy_scene: PackedScene
 
@@ -13,5 +13,5 @@ func on_timer_timeout() -> void:
 	if not enemy_scene:
 		return
 	var enemy_inst = enemy_scene.instantiate()
-	enemy_inst.global_position = marker_2d.global_position
+	enemy_inst.global_position = marker_2d.global_position 
 	add_child(enemy_inst, true)
