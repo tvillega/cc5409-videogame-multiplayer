a3b79d37d70e3c23d77abf0bb20e0f18ea8bf506
Tom Villegas <tvillega@dcc.uchile.cl>
2024-11-25 22:15:46 -0300

    fix: spawn chibi enemies on a cosine re-location
    This is to prevent the freaking overflow of enemies that crashes everything

--- scenes/enemies/skel_spawner.gd
+++ scenes/enemies/skel_spawner.gd
@@ -3,6 +3,7 @@ extends MultiplayerSpawner
 @onready var marker_2d: Marker2D = $SkelSpawnerMarker
 @onready var timer: Timer = $Timer
 @export var enemy_scene: PackedScene
+@export var count = 0
 
 func _ready() -> void:
 	#if multiplayer.is_server():
@@ -13,5 +14,7 @@ func on_timer_timeout() -> void:
 	if not enemy_scene:
 		return
 	var enemy_inst = enemy_scene.instantiate()
-	enemy_inst.global_position = marker_2d.global_position 
+	count+=500
+	enemy_inst.global_position = marker_2d.global_position + Vector2(sin((PI * count) / 30) * 20 / 2 * PI, sin((PI * count) / 30) * 20 / 2 * PI)
+	count = count%5000
 	add_child(enemy_inst, true)
