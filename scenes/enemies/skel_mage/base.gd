7c33161950f34ebc3abac8cb27ed2b1748bb159e
Tom Villegas <tvillega@dcc.uchile.cl>
2024-12-04 21:32:29 -0300

    fix adjust skel mage dificulty, but broke the hitbox

--- scenes/enemies/skel_mage/base.gd
+++ scenes/enemies/skel_mage/base.gd
@@ -8,11 +8,16 @@ func _ready() -> void:
 	timer.timeout.connect(_on_spell_timer_timeout)
 	timer.start()
 	%SpellShootingPoint.global_position += Vector2(1,1)
+	
+func _physics_process(delta):
+	pass
 
 func _on_spell_timer_timeout() -> void:
-	var projectile_scene = preload("res://scenes/enemies/skel_mage/fire_ball.tscn")
-	var new_projectile = projectile_scene.instantiate()
-	new_projectile.global_position = %SpellShootingPoint.global_position
-	var world = get_tree().current_scene
-	world.add_child(new_projectile)
-	#add_child(new_projectile)
+	if target:
+		var projectile_scene = preload("res://scenes/enemies/skel_mage/fire_ball.tscn")
+		var new_projectile = projectile_scene.instantiate()
+		new_projectile.global_position = %SpellShootingPoint.global_position
+		new_projectile.global_rotation = global_position.direction_to(target.global_position).angle()
+		var world = get_tree().current_scene
+		world.add_child(new_projectile)
+		#add_child(new_projectile)
